bd7f2db7cd1572e36867bbbe5fbef5babfb92e94
Andrey <AimDragMan2@yandex.ru>
2020-09-28 01:48:06 +0300

    includes

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+$config = array(
+	'title' => 'Новости',
+	'db' => array(
+		'server' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'name' => 'news_db'
+	)
+);
+
+require "db.php";
